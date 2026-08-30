ee568569c5308ee1c159ac0dfe893b6a56560ec3
stachwk <33524981+stachwk@users.noreply.github.com>
2026-08-31 00:21:20 +0200

    Listen on container interfaces for replica lab

--- docker/replica-read/replica-entrypoint.sh
+++ docker/replica-read/replica-entrypoint.sh
@@ -35,5 +35,6 @@ chown -R postgres:postgres /var/lib/postgresql/data
 chmod 700 "$PGDATA"
 
 exec /usr/local/bin/docker-entrypoint.sh postgres \
+    -c listen_addresses='*' \
     -c hot_standby=on \
     -c hot_standby_feedback=off
